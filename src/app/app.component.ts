9091a0db537ef724d869d2657779cd76855ef698
ashanwithana <ashanwithana4@gmail.com>
2025-03-15 13:29:09 +0530

    Add form component for user input handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CounterComponent } from "./components/counter/counter.component";
+import { FormComponent } from "./components/form/form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ CounterComponent],
+  imports: [CounterComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
