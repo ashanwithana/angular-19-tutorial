754753c811ebc94d1c2a3efc64df0e3b5831bd0f
ashanwithana <ashanwithana4@gmail.com>
2025-03-15 14:04:04 +0530

    Add control flow component and update app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CounterComponent } from "./components/counter/counter.component";
 import { FormComponent } from "./components/form/form.component";
+import { ControlFlowComponent } from "./components/control-flow/control-flow.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CounterComponent, FormComponent],
+  imports: [CounterComponent, FormComponent, ControlFlowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
