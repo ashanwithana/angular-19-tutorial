f6832152f1b4831fbccba31f75663ff1436ea248
ashanwithana <ashanwithana4@gmail.com>
2025-03-18 14:13:13 +0530

    Add template-driven forms implementation and update form component structure

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,17 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, NgIf, FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
 })
@@ -29,28 +36,36 @@ export class FormComponent {
   }
 
   setValue() {
-    this.name.setValue('peter')
-    this.password.setValue('1234')
-    this.email.setValue('test@test.com')
+    this.name.setValue('peter');
+    this.password.setValue('1234');
+    this.email.setValue('test@test.com');
   }
 
   detailsForm = new FormGroup({
-    fname : new FormControl('',[Validators.required]),
-    fpassword : new FormControl('',[Validators.required, Validators.minLength(8)]),
-    femail : new FormControl('',[Validators.required, Validators.email]),
-  })
+    fname: new FormControl('', [Validators.required]),
+    fpassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    femail: new FormControl('', [Validators.required, Validators.email]),
+  });
 
-  submitData(){
+  submitData() {
     console.log(this.detailsForm.value);
   }
 
-  get fname(){
-    return this.detailsForm.get('fname')
+  get fname() {
+    return this.detailsForm.get('fname');
   }
-  get fpassword(){
-    return this.detailsForm.get('fpassword')
+  get fpassword() {
+    return this.detailsForm.get('fpassword');
   }
-  get femail(){
-    return this.detailsForm.get('femail')
+  get femail() {
+    return this.detailsForm.get('femail');
+  }
+
+  userData: any;
+  addUser(val: NgForm) {
+    this.userData = val;
   }
 }
